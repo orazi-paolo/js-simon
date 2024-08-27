eb02c6e1100861fafcb718aa504c707684aa3bd1
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-27 15:58:25 +0200

    added list number && change screen when timer ends

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Prendo dal dom gli elementi che mi servono
 const countdownField = document.getElementById('countdown');
+const guessNumber = document.getElementById('guessNumber');
+const inputUser = document.getElementById('input-user');
+const description = document.getElementById('description');
+const buttonSendNumber = document.querySelector('button');
+const valuation = document.getElementById('valuation');
+
 
 // Creo una variabile per il tempo rimanente
-let timeLeft = 5;
+let timeLeft = 2;
 
 // Creo la funzione per il countdown
 function countdown() {
@@ -14,9 +20,21 @@ function countdown() {
         clearInterval(countdownInterval);
         // faccio sparire il tempo rimanente
         countdownField.classList.add('d-none');
+        // faccio sparire i numeri da ricordarsi
+        guessNumber.classList.add('d-none');
+        // tolgo la descrizione
+        description.classList.add('d-none');
+        // rendo visibili gli input per il giocatore
+        inputUser.classList.remove('d-none');
+        // e rendo infine visibili il bottone e la spiegazione di cosa fare
+        buttonSendNumber.classList.remove('d-none');
+        valuation.classList.remove('d-none');
     }
 }
 
 // Creo l'intervallo
 const countdownInterval = setInterval(countdown, 1000);
+
+// creo un array dei numeri scritti in pagina
+const guessNumberArray = [16, 45, 53, 67, 91];
 
